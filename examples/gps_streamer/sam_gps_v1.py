84ceb85494495a76ee99040d1a9476b26033f55d
Brad Lisien <blisien@nrec.ri.cmu.edu>
2022-04-13 15:40:45 -0400

    GPS fields udpated, pretty good with most of info we want now

--- examples/gps_streamer/sam_gps_v1.py
+++ examples/gps_streamer/sam_gps_v1.py
@@ -40,6 +40,40 @@ port = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
 gps = UbloxGps(port)
 
 
+'''
+Message UBX-NAV2-VELNED
+Velocity solution in NED frame
+
+Type Periodic/polled
+
+Comment See important comments concerning validity of position given in section Navigation output filters in the
+integration manual.
+
+'''
+
+
+
+'''
+Message NMEA-NAV2-GGA
+Global positioning system fix data
+Type Output
+Comment Time and position, together with GPS fixing-related data (number of satellites in use, and the resulting HDOP,
+age of differential data if in use, etc.).
+To identify the navigation data source for NMEA Secondary filter output, the alphanumeric string source-
+identification (s:) parameter is used in a TAG Block, in respect to NMEA 0183 Standard.
+☞ The output of this message is dependent on the currently selected datum (default: WGS84). The NMEA
+specification indicates that the GGA message is GPS-specific. However, when the receiver is configured for
+multi-GNSS, the GGA message contents will be generated from the multi-GNSS solution. For multi-GNSS
+use, it is recommended that the NMEA-GNS message is used instead.
+'''
+
+'''
+.9.2.2 Lat/Long position data
+Message NMEA-PUBX-POSITION
+Lat/Long position data
+'''
+
+
 
 try:
     print("Listening for UBX Messages")
@@ -50,7 +84,22 @@ try:
             geo = gps.geo_coords()
             print("Longitude: ", geo.lon) 
             print("Latitude: ", geo.lat)
+            print("Height (above ellipsoid): ", geo.height)
+            print("Height (above mean sea level): ",geo.hMSL)
+
+            print("Accuracy error estimate (Horizontial) [mm]:",geo.hAcc)
+            print("Accuracy error estimate (Vertical) [mm]:",geo.vAcc)
+
+            print("NED north velocity [mm/s]",geo.velN)
+            print("NED east  velocity [mm/s]",geo.velE)
+            print("NED down  velocity [mm/s]",geo.velD)
+            
+            print("Ground speed 2D [mm/s]",geo.gSpeed)            
             print("Heading of Motion: ", geo.headMot)
+
+            print("Position pDOP:",geo.pDOP)
+
+            print(" Number of satellites used to get position =>"+str(geo.numSV))
 
             print("="*30)
         
@@ -67,6 +116,15 @@ try:
             print("="*30)
 
             gps_status = geo
+
+            #print("Last correction time =>"+str(gps_status.flags3.lastCorrectionAge))
+            print("fixType (GPS health) =>"+str(gps_status.fixType))
+            print("fix status - valid fix? =>"+str(gps_status.flags.gnssFixOK))
+            print("fix status - differential correction used? =>"+str(gps_status.flags.diffSoln))
+            print("fix status - power mode =>"+str(gps_status.flags.psmState))
+            print("fix status - heading valid? =>"+str(gps_status.flags.headVehValid))
+
+            
             
             #global sat_info
             #sat_info = gps.satellites()
